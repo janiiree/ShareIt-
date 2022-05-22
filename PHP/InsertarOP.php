42a88736645b1c05a808b4e175d04c899fc80a68
Jon Barbero <49266575+JonBarbero@users.noreply.github.com>
2022-05-23 01:10:58 +0200

    Add files via upload

--- PHP/InsertarOP.php
+++ PHP/InsertarOP.php
@@ -5,11 +5,11 @@ $username_localhost="Xjbarbero004";
 $password_localhost="*0My0ui41hu";
 
 $json=array();
-	$descripcion=$_GET["nombre"];
-	$email=$_GET["correo"];
-	$url=$_GET["descripcion"];
-	$titulo=$_GET["ep"];
-	$estado=$_GET["imgURL"];
+	$descripcion=$_GET["descripcion"];
+	$email=$_GET["email"];
+	$url=$_GET["url"];
+	$titulo=$_GET["titulo"];
+	$estado=$_GET["estado"];
 
 	$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 	$consulta="insert into objetosperdidos(descripcion, email, url, titulo, estado) values ('$descripcion', '$email', '$url', '$titulo', '$estado');
