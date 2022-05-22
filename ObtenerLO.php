ed7b688e8d69b9d095482f58081ac87c9fa60ba9
Jon Barbero <49266575+JonBarbero@users.noreply.github.com>
2022-05-22 16:37:12 +0200

    cambios en obtenerLO

--- ObtenerLO.php
+++ ObtenerLO.php
@@ -1,20 +1,34 @@
 <?PHP
 $hostname_localhost="localhost";
-$database_localhost="Xjbarbero004_shareit";
-$username_localhost="Xjbarbero004";
-$password_localhost="*0My0ui41hu";
+$database_localhost="Xgdelrio004_tareas";
+$username_localhost="Xgdelrio004";
+$password_localhost="*Q48K5Cupz";
 
 $json=array();
-	
-		$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
-
-		$consulta="select * from objetosperdidos";
-		$resultado=mysqli_query($conexion,$consulta);
+	$propietario=$_GET["propietario"];
+	$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
+	$consulta="select * from objetosperdidos";
+	$resultado=mysqli_query($conexion,$consulta);
 		
+		
+	if ($registro=mysqli_fetch_array($resultado)){	
+		$json['desc'][]=$registro[1];
+		$json['email'][]=$registro[2];
+		$json['url'][]=$registro[3];
+		$json['titulo'][]=$registro[4];
 		while($registro=mysqli_fetch_array($resultado)){
-			$json['objetosperdidos'][]=$registro;
+			$json['desc'][]=$registro[1];
+			$json['email'][]=$registro[2];
+			$json['url'][]=$registro[3];
+			$json['titulo'][]=$registro[4];
 		}
-		mysqli_close($conexion);
 		echo json_encode($json);
+	} else{
+		echo "no hay tareas";
+	}
+	mysqli_close($conexion);
+	
+
+
 
 ?>
