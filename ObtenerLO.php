45f1cd119bc21ff4731874ddd4a7676ea5a3cd88
Jon Barbero <49266575+JonBarbero@users.noreply.github.com>
2022-05-22 16:42:35 +0200

    Update ObtenerLO.php

--- ObtenerLO.php
+++ ObtenerLO.php
@@ -1,15 +1,13 @@
 <?PHP
 $hostname_localhost="localhost";
-$database_localhost="Xjbarbero004_objetosperdidos";
+$database_localhost="Xjbarbero004_shareit";
 $username_localhost="Xjbarbero004";
 $password_localhost="*0My0ui41hu";
 
 $json=array();
-	$propietario=$_GET["propietario"];
 	$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 	$consulta="select * from objetosperdidos";
 	$resultado=mysqli_query($conexion,$consulta);
-		
 		
 	if ($registro=mysqli_fetch_array($resultado)){	
 		$json['desc'][]=$registro[1];
@@ -24,11 +22,8 @@ $json=array();
 		}
 		echo json_encode($json);
 	} else{
-		echo "no hay tareas";
+		echo "No hay objetos perdidos";
 	}
 	mysqli_close($conexion);
 	
-
-
-
 ?>
